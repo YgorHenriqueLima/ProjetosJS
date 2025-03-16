dc3b86aed7f617f0ced477be7fdbd69c4695a4cf
Ygorhenrique <118612433+YgorHenriqueLima@users.noreply.github.com>
2025-03-16 11:50:38 -0300

    terminando a tabela e a adição interativa dos dados na tabela

--- DataGridView/script.js
+++ DataGridView/script.js
@@ -1,20 +1,19 @@
 /*
     passos:
-
     javascript vai adicionar o conteúdo ao dataGridView que vai estar previamente construído no HTML
-
     depois para ara a ideia de módulo, onde todo o html seja construído pelo m´odulo 
 */
-
 /*objeto de configuração do DataGridView*/
+
 const configDGV ={
     endpoint:"produtos.json",
     idDestino:"dgv_dados",
 }
 
-
 const DataGridView=(configdgv)=>{
     const dgvDados = document.getElementById(configDGV.idDestino);
+    dgvDados.innerHTML = "";
+    /*  Buscando os dados do endpoint produtos.json */
     fetch(configDGV.endpoint)
     .then(res=>res.json())
     .then(res=>{
@@ -22,48 +21,51 @@ const DataGridView=(configdgv)=>{
             const dgvLinha = document.createElement("tr");
             dgvLinha.setAttribute("class","dgvLinha");
 
-            const c1 = document.createElement("td");
-            c1.setAttribute("class","c1");
-            c1.innerHTML = `${element.id}`;
-            dgvLinha.appendChild(c1);
-
-            const c2 = document.createElement("td");
-            c2.setAttribute("class","c2");
-            c2.innerHTML = `${element.produto}`;
-            dgvLinha.appendChild(c2);
-
-            const c3 = document.createElement("td");
-            c3.setAttribute("class","c3");
-            c3.innerHTML = `${element.marca}`;
-            dgvLinha.appendChild(c3);
-
-            const c4 = document.createElement("td");
-            c4.setAttribute("class","c4");
-            c4.innerHTML = `${element.modelo}`;
-            dgvLinha.appendChild(c4);
+            /*SIMPLIFICANDO O CÓDIGO
+                primeiro criando uma constante que tem as colunas dentro de uma array
+            */
+           
+            const colunas = ["id","produto","marca","modelo"];
+            
+            /**
+             *  criando um forEach da array coluna para simplificar o código de cada coluna
+             */
+            colunas.forEach(chave =>{
+                const coluna = document.createElement("td");
+                coluna.classList.add(`c${chave}`);
+                coluna.textContent = element[chave];
+                dgvLinha.appendChild(coluna);
+            });
 
             const c5 = document.createElement("td");
             c5.setAttribute("class","c5");
-            c5.innerHTML = `D E V`;
             dgvLinha.appendChild(c5);
 
+            /* 
+            CRIANDO O ÍCONE DE LIXEIRA PARA APAGAR O DADO NA TABELA 
+            */
+            const imgDelete = document.createElement("img");
+            imgDelete.setAttribute("class","dgvIcone");
+            imgDelete.setAttribute("src","./svg/deletar.svg");
+            c5.appendChild(imgDelete);
+
+            /*criando o ícone de editar para fazer mudanças nos dados*/
+
+            const imgEditar = document.createElement("img");
+            imgEditar.setAttribute("class","dgvIcone");
+            imgEditar.setAttribute("src","./svg/editar.svg")
+            c5.appendChild(imgEditar);
+
+            const imgExibir = document.createElement("img");
+            imgExibir.setAttribute("class","dgvIcone");
+            imgExibir.setAttribute("src","./svg/exibir.svg");
+            c5.appendChild(imgExibir)
+
             dgvDados.appendChild(dgvLinha);
-            
-            console.log(res)
+            console.log(res);
             
         });
     });
 }
 
 DataGridView(configDGV);
-
-/**
- *  <tr class="dgvLinha">
-                <td class="c1">01</td>
-                <td class="c2">Processador</td>
-                <td class="c3">Intel</td>
-                <td class="c4">I7</td>
-                <td class="c5">D E V</td>
-            </tr>
- * 
- * */ 
